95c3aa93191d8ba2f1298f9f24f4a04b1681e0d1
Jesse B. Miller <jesse@jessebmiller.com>
2021-08-23 14:57:42 -0500

    WIP

--- polywrap/test/query.spec.ts
+++ polywrap/test/query.spec.ts
@@ -2,46 +2,75 @@ import { Web3ApiClient } from '@web3api/client-js'
 import { ethereumPlugin } from "@web3api/ethereum-plugin-js"
 import { ipfsPlugin } from "@web3api/ipfs-plugin-js"
 import { ensPlugin } from "@web3api/ens-plugin-js"
+import { graphNodePlugin } from "@web3api/graph-node-plugin-js"
+import axios from 'axios'
 
-// fetch providers from dev server
-const {
-  data: { ipfs, ethereum },
-} = await axios.get("http://localhost:4040/providers");
+jest.setTimeout(120000)
 
-if (!ipfs) {
-  throw Error("Dev server must be running at port 4040");
+async function getClient() {
+  // fetch providers from dev server
+  const {
+    data: { ipfs, ethereum },
+  } = await axios.get("http://localhost:4040/providers");
+
+  if (!ipfs) {
+    throw Error("Dev server must be running at port 4040");
+  }
+
+  const ensConfig = await axios.get("http://localhost:4040/ens")
+  const ensAddress = ensConfig.data.ensAddress
+
+  return new Web3ApiClient({
+    plugins: [
+      {
+        uri: "/ens/ens.web3api.eth",
+        plugin: ensPlugin({
+          addresses: {
+            testnet: ensAddress
+          }
+        })
+      },
+      {
+        uri: "/ens/ethereum.web3api.eth",
+        plugin: ethereumPlugin({
+          networks: {
+            testnet: {
+              provider: ethereum,
+              signer: 0
+            }
+          },
+          // If defaultNetwork is not specified, mainnet will be used.
+          defaultNetwork: "testnet"
+        })
+      },
+      {
+        uri: "/ens/ipfs.web3api.eth",
+        plugin: ipfsPlugin({
+          provider: "http://ipfs.io",
+          fallbackProviders: [ipfs]
+        }),
+      },
+      {
+        uri: 'ens/graph-node.web3api.eth',
+        plugin: graphNodePlugin({
+          provider: "http://127.0.0.1:8000/"
+        })
+      }
+    ]
+  })
 }
 
-const client = new Web3ApiClient({
-  redirects: [
-    {
-      from: "ens/ethereum.web3api.eth",
-      to: ethereumPlugin({
-        networks: {
-          testnet: {
-            provider: ethereum
-          },
-          MAINNET: {
-            provider: "http://localhost:8546"
-          },
-        },
-        defaultNetwork: "testnet"
-      })
-    },
-    {
-      from: "w3://ens/ipfs.web3api.eth",
-      to: ipfsPlugin({ provider: ipfs }),
-    },
-    {
-      from: "w3://ens/ens.web3api.eth",
-        to: ensPlugin({ addresses: { testnet: ensAddress } }),
-    }
-  ],
-  tracingEnabled: true
+let client: Web3ApiClient
+beforeAll(async () => {
+  client = await getClient()
 })
 
 test("client can query the graph", async () => {
-  const response = await client.query(
-    uri: 'ens/testnet/squadprotocol.eth',
-  )
+  const response = await client.query({
+    uri: '/ens/testnet/squadprotocol.eth',
+    query: `query queryGraphNode {
+queryGraphNode(query: "{  squadNFTs {    creator    blockCreated  }}")
+}`
+  })
+  expect(response.errors).toBe(undefined)
 })
