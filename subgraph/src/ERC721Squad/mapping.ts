de8545336a7a458e5fb5af589329d19ccb65010b
Ezra Weller <ezra.weller@gmail.com>
2021-08-24 23:27:21 -0400

    fixes

--- subgraph/src/ERC721Squad/mapping.ts
+++ subgraph/src/ERC721Squad/mapping.ts
@@ -1,7 +1,6 @@
 import { TokenMinted } from '../../generated/ERC721Squad/ERC721Squad'
 import { SquadNFT } from '../../generated/schema'
 
-// should now create NFT and SquadNftData, not SquadNFT
 export function handleTokenMinted(event: TokenMinted): void {
   let nft = new SquadNFT(event.params.tokenId.toString())
   nft.creator = event.params.creator
--- subgraph/src/PurchasableLicenseManager/mapping.ts
+++ subgraph/src/PurchasableLicenseManager/mapping.ts
@@ -1,50 +1,27 @@
-import { store, Address, BigInt, json, ByteArray, Bytes, log, JSONValueKind } from "@graphprotocol/graph-ts"
+import { store, log } from "@graphprotocol/graph-ts"
 import { NFTRegistered, NFTUnregistered, Purchase } 
   from '../../generated/PurchasableLicenseManager/PurchasableLicenseManager'
-import { Content, PurchasableLicense, PurchaseEvent } from '../../generated/schema'
+import { PurchasableLicense, PurchaseEvent } from '../../generated/schema'
+import { makeContentId, makeLicenseId, registrationContentAndLicenseId } from '../utils'
 
-function makeContentId(address: Address, id: BigInt): string {
-  return address.toHex() + '-' + id.toHex()
-}
-
-function makeLicenseId(contentId: string, address: Address): string {
-  return contentId + '-' + address.toHex()
-}
-
-// should now also create or update an NFT with a type
-export function handleNFTRegistered(event: NFTRegistered): void {
-  let contentId = makeContentId(event.params.nftAddress, event.params.nftId)
-  let content = Content.load(contentId)
-  if (content == null) {
-    content = new Content(contentId)
-    content.nftAddress = event.params.nftAddress
-    content.nftId = event.params.nftId
-  }
-
-  let result = json.try_fromBytes(
-    ByteArray.fromUTF8(event.params.data) as Bytes
+export function handleNFTRegisteredPurchasable(event: NFTRegistered): void {
+  let result = registrationContentAndLicenseId(
+    event.address, 
+    event.params.nftAddress,
+    event.params.nftId,
+    event.params.data
   )
-  if (result.isError) { 
-    log.warning("Failed to parse JSON", [])
+  if (result.error != "none") {
+    log.error(result.error, [])
     return
   }
+  let licenseId = result.licenseId
 
-  let object = result.value.toObject()
-  let typeValue = object.get("type")
-  if (typeValue.kind != JSONValueKind.STRING) { 
-    log.warning("Skipping post: missing valid Postum 'type' field", [])
-    return
-  }
-  content.type = typeValue.toString()
-  
-  content.save()
-
-  let licenseId = makeLicenseId(contentId, event.address)
   let purchasableLicense = PurchasableLicense.load(licenseId)
   if (purchasableLicense == null) {
     purchasableLicense = new PurchasableLicense(licenseId)
     purchasableLicense.licenseManagerAddress = event.address
-    purchasableLicense.content = contentId
+    purchasableLicense.content = result.contentId
   }
   purchasableLicense.registrant = event.transaction.from
   purchasableLicense.price = event.params.price
