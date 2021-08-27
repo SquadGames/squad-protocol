042ac4f7fa2785c1505fac509bca4a968d3054f8
Jesse B. Miller <jesse@jessebmiller.com>
2021-08-26 21:05:02 -0500

    merkle tree library

--- lib/src/merkle.ts
+++ lib/src/merkle.ts
@@ -1,46 +1,110 @@
-import MerkleTree from 'merkletreejs'
-import { BigNumber, utils } from 'ethers'
+import { bufferToHex, keccak256 } from 'ethereumjs-util'
 
-export { MerkleTree }
-
-export interface Balance { account: string; allocation: BigNumber; }
-
-export function createBalanceTree(balances: Balance[]): MerkleTree {
-  return new MerkleTree(
-    balances.map(b => { return toLeaf(b)}),
-    utils.keccak256,
-    { sortPairs: true }
-  )
+interface ProofMap {
+  [key: string]: Buffer[]
 }
 
-export function getProof(balanceTree: MerkleTree, balance: Balance): Buffer[] {
-  return balanceTree.getProof(
-    toLeaf(balance)
-  )
+export interface MerkleTree {
+  data: Buffer // should be 32 bytes not sure how to guarantee that with types
+  children: [MerkleTree, MerkleTree] | null
+  proofs: ProofMap
 }
 
-export function getHexProof(balanceTree: MerkleTree, balance: Balance): string[] {
-  return balanceTree.getHexProof(
-    toLeaf(balance)
-  )
+export const hashFn = keccak256
+
+function hashPair (left: Buffer, right: Buffer): Buffer {
+  return hashFn(Buffer.concat([left, right].sort(Buffer.compare)))
 }
 
-export function verifyProof(
-  balanceTree: MerkleTree,
-  balance: Balance,
-  root: string | Buffer,
-  proof: string[] | Buffer[]
-): boolean {
-  return balanceTree.verify(
-    proof,
-    toLeaf(balance),
-    root
-  )
+function extendProofs (left: MerkleTree, right: MerkleTree): ProofMap {
+  const proofMap: ProofMap = {}
+  for (const leaf in left.proofs) {
+    const proof = left.proofs[leaf]
+    if (proof === undefined) {
+      throw new Error('expected proof to be defined')
+    }
+    proofMap[leaf] = [...proof, right.data]
+  }
+  for (const leaf in right.proofs) {
+    const proof = right.proofs[leaf]
+    if (proof === undefined) {
+      throw new Error('expected proof to be defined')
+    }
+    proofMap[leaf] = [...proof, left.data]
+  }
+  return proofMap
 }
 
-function toLeaf (balance: Balance): string {
-  return utils.solidityKeccak256(
-    ['address', 'uint256'], 
-    [balance.account, balance.allocation]
+function _makeMerkleTree (layer: MerkleTree[]): MerkleTree[] {
+  if (layer.length === 0) {
+    throw new Error('Empty tree')
+  }
+  if (layer.length === 1) {
+    // this is the root
+    return layer
+  }
+  const nextLayer: MerkleTree[] = []
+  for (let i = 0; i < layer.length; i += 2) {
+    const left: MerkleTree | undefined = layer[i]
+    if (left === undefined) {
+      throw new Error('layer index out of bounds error')
+    }
+    const right: MerkleTree = layer[i + 1] ?? left
+    nextLayer.push({
+      data: hashPair(left.data, right.data),
+      children: [left, right],
+      proofs: extendProofs(left, right)
+    })
+  }
+  return _makeMerkleTree(nextLayer)
+}
+
+export function makeMerkleTree (leaves: Buffer[]): MerkleTree {
+  // remove duplicate leaves and sort them
+  leaves = [...new Set(leaves)].sort(Buffer.compare)
+
+  // create MerkleTree leaves from Buffer leaves
+  const preparedLeaves: MerkleTree[] = leaves.map(
+    (l: Buffer): MerkleTree => {
+      const proofs: ProofMap = {}
+      proofs[l.toString('hex')] = []
+      return { data: l, children: null, proofs }
+    }
   )
+
+  const tree = _makeMerkleTree(preparedLeaves)[0]
+  if (tree === undefined) {
+    throw new Error('Could not create merkle tree')
+  }
+  return tree
+}
+
+export function getRoot (tree: MerkleTree): Buffer {
+  return tree.data
+}
+
+export function getHexRoot (tree: MerkleTree): string {
+  return bufferToHex(getRoot(tree))
+}
+
+export function getProof (tree: MerkleTree, leaf: Buffer): Buffer[] {
+  const proof = tree.proofs[leaf.toString('hex')]
+  if (proof === undefined) {
+    throw new Error('leaf not in tree')
+  }
+  return proof
+}
+
+export function getHexProof (tree: MerkleTree, leaf: string): string[] {
+  return getProof(tree, Buffer.from(leaf, 'hex')).map(bufferToHex)
+}
+
+export function verify (proof: Buffer[], root: Buffer, leaf: Buffer): boolean {
+  const check = proof.reduce(
+    (currentHash: Buffer, nextElement: Buffer): Buffer => {
+      return hashPair(currentHash, nextElement)
+    },
+    leaf
+  )
+  return check.equals(root)
 }
--- lib/test/test.ts
+++ lib/test/test.ts
@@ -1,34 +1,91 @@
 import * as lib from '../src'
 import * as fs from 'fs'
 
-test('getConfig returns a config object', () => {
-  process.env.SQUAD_CONFIG_PATH = 'test/test-config.json'
-  process.env.SQUAD_SECRETS_DIR = 'test'
-  const config = lib.getConfig()
-  expect(config.testConfigKey).toBe('testConfigValue')
+function randInt (max: number): number {
+  return Math.floor(Math.random() * max)
+}
+
+describe('MerkleTree', () => {
+  // leaves, tree, root
+  let cases: Array<[Buffer[], lib.MerkleTree, Buffer]>
+  beforeEach(() => {
+    cases = [...Array(200)].map((x) => {
+      const leaves = [...Array(randInt(100) + 2)].map((i) => {
+        return Buffer.from(Math.random().toString())
+      })
+      const tree = lib.makeMerkleTree(leaves)
+      return [leaves, tree, lib.getRoot(tree)]
+    })
+  })
+
+  test('It generates and validates proofs', () => {
+    cases.forEach(([leaves, tree, root]) => {
+      leaves.forEach((leaf) => {
+        const proof = lib.getProof(tree, leaf)
+        expect(lib.verify(proof, root, leaf)).toBe(true)
+      })
+    })
+  })
+
+  test('Root is 32 bytes', () => {
+    cases.forEach(([leaves, tree, root]) => {
+      const length = lib.getRoot(tree).length
+      if (length !== 32) {
+        console.log('leaves', leaves)
+        console.log('tree', tree)
+        console.log('root', root)
+      }
+      expect(lib.getRoot(tree).length).toBe(32)
+    })
+  })
+
+  test('Generates expected root', () => {
+    const leaves = [
+      Buffer.from('a'),
+      Buffer.from('b'),
+      Buffer.from('c')
+    ]
+    const expectedRoot = lib.hashFn(
+      Buffer.concat([
+        lib.hashFn(Buffer.from('ab')),
+        lib.hashFn(Buffer.from('cc'))
+      ])
+    )
+    const tree = lib.makeMerkleTree(leaves)
+    expect(lib.getRoot(tree)).toStrictEqual(expectedRoot)
+  })
 })
 
-test('getSecrets returns a network specific secrets object', () => {
-  process.env.SQUAD_CONFIG_PATH = 'test/test-config.json'
-  process.env.SQUAD_SECRETS_DIR = 'test'
-  const secrets = lib.getSecrets()
-  expect(secrets.mockSecretKey).toBe('mock-secret-value')
-})
+describe('Config', () => {
+  test('getConfig returns a config object', () => {
+    process.env.SQUAD_CONFIG_PATH = 'test/test-config.json'
+    process.env.SQUAD_SECRETS_DIR = 'test'
+    const config = lib.getConfig()
+    expect(config.testConfigKey).toBe('testConfigValue')
+  })
 
-test('writeConfig writes config', () => {
-  process.env.SQUAD_SECRETS_DIR = 'test'
-  process.env.SQUAD_CONFIG_PATH = 'test/write-test-config.json'
-  const value = Date.now().toString()
-  const config = {
-    writeTestKey: value,
-    network: 'localhost',
-    networkNameOrUrl: '',
-    contracts: {}
-  }
+  test('getSecrets returns a network specific secrets object', () => {
+    process.env.SQUAD_CONFIG_PATH = 'test/test-config.json'
+    process.env.SQUAD_SECRETS_DIR = 'test'
+    const secrets = lib.getSecrets()
+    expect(secrets.mockSecretKey).toBe('mock-secret-value')
+  })
 
-  lib.writeConfig(config, value)
-  expect(lib.getConfig().writeTestKey).toBe(value)
-  const localFile = `../config/localhost-${value}.json`
-  expect(fs.existsSync(localFile)).toBe(true)
-  fs.unlinkSync(localFile)
+  test('writeConfig writes config', () => {
+    process.env.SQUAD_SECRETS_DIR = 'test'
+    process.env.SQUAD_CONFIG_PATH = 'test/write-test-config.json'
+    const value = Date.now().toString()
+    const config = {
+      writeTestKey: value,
+      network: 'localhost',
+      networkNameOrUrl: '',
+      contracts: {}
+    }
+
+    lib.writeConfig(config, value)
+    expect(lib.getConfig().writeTestKey).toBe(value)
+    const localFile = `../config/localhost-${value}.json`
+    expect(fs.existsSync(localFile)).toBe(true)
+    fs.unlinkSync(localFile)
+  })
 })
