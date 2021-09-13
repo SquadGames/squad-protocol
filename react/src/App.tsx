96f5c0d47e7af01ad09657e1809d41bf954f1230
Jesse B. Miller <jesse@jessebmiller.com>
2021-09-13 14:36:28 -0500

    polywrap import error

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,17 +1,87 @@
 import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import axios from 'axios'
 
 import { ContentPage } from './content'
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client"
+
+//import { ethereumPlugin } from "@web3api/ethereum-plugin-js"
+//import { ipfsPlugin } from "@web3api/ipfs-plugin-js"
+//import { ensPlugin } from "@web3api/ens-plugin-js"
+//import { Web3ApiProvider } from '@web3api/react'
+
+const apolloClient = new ApolloClient({
+  uri: 'http://localhost:8000/subgraphs/name/squadgames/squad-POC-subgraph',
+  cache: new InMemoryCache()
+})
+
+function getPolywrapPlugins() {
+  return "get polywrap plugins"
+  // fetch providers from dev server
+  const ipfs = "http://localhost:5001"
+  const ethereum = "http://localhost:8545"
+  const ensAddress = "0x630589690929E9cdEFDeF0734717a9eF3Ec7Fcfe"
+
+  return [
+    {
+      uri: "/ens/ens.web3api.eth",
+      plugin: ensPlugin({
+        addresses: {
+          testnet: ensAddress
+        }
+      })
+    },
+    {
+      uri: "/ens/ethereum.web3api.eth",
+      plugin: ethereumPlugin({
+        networks: {
+          testnet: {
+            provider: ethereum,
+            signer: signer
+          }
+        },
+        // If defaultNetwork is not specified, mainnet will be used.
+        defaultNetwork: "testnet"
+      })
+    },
+    {
+      uri: "/ens/ipfs.web3api.eth",
+      plugin: ipfsPlugin({
+        provider: ipfs
+      }),
+    },
+    {
+      uri: 'ens/graph-node.web3api.eth',
+      plugin: graphNodePlugin({
+        provider: "http://localhost:8000"
+      })
+    }
+  ]
+}
+
 function App() {
+  return <p>{Math.random()}</p>
   const [itemsPerPage, setItemsPerPage] = useState(25)
   const [pageNumber, setPageNumber] = useState(0)
+  const [polywrapPlugins, setPolywrapPlugins] = useState([])
 
   function handleItemsPerPageChange(event) {
     const value = event.target.value
     setItemsPerPage(value)
   }
+  console.log(polywrapPlugins)
+  if (polywrapPlugins.length === 0) {
+//    console.log(getPolywrapPlugins())
+    return <p>Loading Polywrap Plugins...</p>
+  }
+
+  console.log(polywrapPlugins)
 
   return (
     <div className="App">
@@ -19,7 +89,13 @@ function App() {
         <p>Squad Content Directory</p>
       </header>
 
-      <ContentPage />
+      <ApolloProvider client={apolloClient}>
+        <ContentPage />
+      </ApolloProvider>
+
+      <Web3ApiProvider plugins={polywrapPlugins}>
+        <p> web3 api provided </p>
+      </Web3ApiProvider>
 
     </div>
   )
--- react/src/main.tsx
+++ react/src/main.tsx
@@ -2,24 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
-} from "@apollo/client"
-
-const apolloClient = new ApolloClient({
-  uri: 'http://localhost:8000/subgraphs/name/squadgames/squad-POC-subgraph',
-  cache: new InMemoryCache()
-})
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={apolloClient}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
